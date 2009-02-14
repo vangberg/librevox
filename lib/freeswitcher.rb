49c8bf57fafd86ecfbaa63d52762a99981dde9eb
bougyman <bougyman@hellspass.nationwide-recovery.com>
2009-02-14 16:21:17 -0600

    added a search for the freeswitch install in FS_INTSALL_PATHS to figure out where freeswitch is installed

--- lib/freeswitcher.rb
+++ lib/freeswitcher.rb
@@ -5,10 +5,7 @@ require 'pp'
 module FreeSwitcher
   # Global configuration options
   #
-  FS_ROOT = "/usr/local/freeswitch".freeze # Location of the freeswitch $${base_dir}
-  FS_CONFIG_PATH = File.join(FS_ROOT, "conf").freeze # Freeswitch conf dir
-  FS_DB_PATH = File.join(FS_ROOT, "db").freeze # Freeswitch db dir
-
+  FS_INSTALL_PATHS = ["/usr/local", "/opt", "/usr"]
   DEFAULT_CALLER_ID_NUMBER = '8675309'
   DEFAULT_CALLER_ID_NAME   = "FreeSwitcher"
 
@@ -36,6 +33,21 @@ module FreeSwitcher
       end
     end
   end
+
+  private
+  def self.find_freeswitch_install
+    FS_INSTALL_PATHS.detect do |path|
+      fs_path = File.join(path, "freeswitch")
+      File.directory?(fs_path) and File.directory?(File.join(fs_path, "conf")) and File.directory?(File.join(fs_path, "db"))
+    end
+  end
+
+  public
+  FS_ROOT = find_freeswitch_install # Location of the freeswitch $${base_dir}
+  raise "Could not find freeswitch root path, searched #{FS_INSTALL_PATHS.join(":")}" if FS_ROOT.nil?
+  FS_CONFIG_PATH = File.join(FS_ROOT, "conf").freeze # Freeswitch conf dir
+  FS_DB_PATH = File.join(FS_ROOT, "db").freeze # Freeswitch db dir
+
 end
 
 $LOAD_PATH.unshift(FreeSwitcher::ROOT)
