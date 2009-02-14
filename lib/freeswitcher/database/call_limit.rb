8797be8c721d1fe4e8d247dd690bed65b579dd31
bougyman <bougyman@hellspass.nationwide-recovery.com>
2009-02-14 11:22:31 -0600

    pulled duplicated code for setting table name

--- lib/freeswitcher/database/call_limit.rb
+++ lib/freeswitcher/database/call_limit.rb
@@ -4,23 +4,14 @@ module FreeSwitcher
     module CallLimit
       DB = Sequel.connect("sqlite:///" + File.join(FreeSwitcher::FS_DB_PATH, "call_limit.db"))
       class LimitData < Sequel::Model
-        def self.table_name
-          :limit_data
-        end
       end
       LimitData.set_dataset :limit_data
 
       class DbData < Sequel::Model
-        def self.table_name
-          :db_data
-        end
       end
       DbData.set_dataset :db_data
 
       class GroupData < Sequel::Model
-        def self.table_name
-          :group_data
-        end
       end
       GroupData.set_dataset :group_data
 
