b368c5bffea87f81099206a47907fb62be9c37be
Jayson Vaughn <jayson@onedrop.home>
2009-02-16 12:08:34 -0600

    Refactored sofia command to use composition for it's features fsr/cmd/sofia.rb for base and fsr/cmd/sofia/*.rb for sofia commandsd
    One can now:
    
      sock.sofia.run
      sock.sofia.status.run
      sock.sofia.status(:status => 'profile', :name => 'internal')
    
    TODO:  Need to add cmd/sofia/profile.rb

--- lib/fsr/cmd/sofia.rb
+++ lib/fsr/cmd/sofia.rb
@@ -2,37 +2,35 @@ require "fsr/app"
 module FSR
   module Cmd
     class Sofia < Command
-      attr_reader :fs_socket
-
-      def initialize(fs_socket = nil, args = {})
-        @fs_socket = fs_socket # FSR::CommandSocket object
-        @status  = args[:status] # Status type; profile or gateway
-
-        # If status is given, make sure it's profile or gateway
-        unless @status.nil?
-          raise "status must be profile or gateway" unless @status =~ /profile|gateway/i
-        end
-
-        @name = args[:name] # Name of profile or gateway
+      def initialize(fs_socket = nil)
+        @fs_socket = fs_socket # FSR::CommandSocket obj
       end
 
-      # Send the command to the event socket, using bgapi by default.
+      # sofia status
+      def status(args = {})
+        require "fsr/cmd/sofia/status" # Require sofia/status
+        Sofia::Status.new(@fs_socket, args)
+      end
+
+      # sofia profile
+      def profile(args = {})
+        require "fsr/cmd/sofia/profile" # Require sofia/profile
+        Sofia::Profile.new(@fs_socket, args)
+      end
+
+      # Send the command to the event socket, using api by default.
       def run(api_method = :api)
         orig_command = "%s %s" % [api_method, raw]
         Log.debug "saying #{orig_command}"
         @fs_socket.say(orig_command)
       end
-
+    
       # This method builds the API command to send to the freeswitch event socket
       def raw
-        if @status and @name
-          orig_command = "sofia status #{@status} #{@name}"
-        else
-          raise "Usage <status> <name> - <status> is profile or gateway.  <name> is name of profile or gateway."
-        end
+        orig_command = "sofia"
       end
     end
 
-    register(:sofia, Sofia) 
+  register(:sofia, Sofia) 
   end
 end
