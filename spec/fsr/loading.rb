5796a3a55a3df0f4afe2e1f7292b703d314a970e
TJ Vanderpoel <bougy.man@gmail.com>
2009-02-17 19:37:13 -0600

    fixed specs to not care about the order of the array, but still only allow commands if they are in the expected command/application set

--- spec/fsr/loading.rb
+++ spec/fsr/loading.rb
@@ -4,15 +4,23 @@ require 'spec/helper'
 describe "Testing FSR module loading methods" do
   # When you add applications  you must modify the expected apps_loaded behavior
   it "Loads all applications" do
+    all_apps = [:conference, :bridge, :fifo] # Add any apps which will load to this set
     apps_loaded = FSR.load_all_applications
     apps_loaded.kind_of?(Array).should == true
-    apps_loaded.should == [:conference, :bridge, :fifo]
+    all_apps.each do |app|
+      apps_loaded.delete(app).should == app
+    end
+    apps_loaded.size.should == 0
   end
 
   # When you add commands  you must modify the expected cmds_loaded behavior
   it "Loads all commands" do
+    all_commands = [:originate, :sofia] # If you add a command add it to this set
     cmds_loaded = FSR.load_all_commands
     cmds_loaded.kind_of?(Array).should == true
-    cmds_loaded.should == [:originate, :sofia]
+    all_commands.each do |cmd|
+      cmds_loaded.delete(cmd).should == cmd
+    end
+    cmds_loaded.size.should == 0
   end
 end
