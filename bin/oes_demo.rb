cf77986bd54b7c5ea11397022d22785c677ebd15
Jayson Vaughn <jayson@onedrop.home>
2009-03-25 16:58:02 -0500

    Updated bin/oes_demo.rb and README to correctly describe how to use FSR::Listener::Outbound

--- bin/oes_demo.rb
+++ bin/oes_demo.rb
@@ -9,10 +9,16 @@ require "fsr/listener/outbound"
 
 module OesDemo
   include FSR::Listener::Outbound
+
   def session_initiated(session)
-    bridge "user/1001"
-    puts "*** #{session.replies}"
+    number = session.headers["Channel-Caller-ID-Number"] # Grab the inbound caller id
+    FSR::Log.info "*** Answering incoming call from #{number}"
+    answer # Answer the call
+    playback 'a_sound_file.wav'
+    bridge "user/1001" # Bridge the call to "user/1001"
+    hangup # Hangup the call
   end
+
 end
 
 FSR.start_oes!(OesDemo, :port => 8084, :host => "localhost")
