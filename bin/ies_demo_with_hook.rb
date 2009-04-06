c3b2df3e25e58642b570b72e0614bb7b1d1247b6
Jayson Vaughn <jayson@onedrop.home>
2009-04-06 15:25:22 -0500

    Updated specs Updated bin/ies_demo_with_hook.rb

--- bin/ies_demo_with_hook.rb
+++ bin/ies_demo_with_hook.rb
@@ -6,17 +6,9 @@ puts $LOAD_PATH.inspect
 $stdout.flush
 require "fsr/listener/inbound"
 
-
-class IesDemo < FSR::Listener::Inbound
-
-  def on_event(event)
-  end
-
-end
-
 # This adds a hook on CHANNEL_CREATE events
-FSR::Listener::Inbound.add_event_hook(:CHANNEL_CREATE) {|event| FSR::Log.info "*** [#{event.content[:unique_id]}} Channel created - greetings from the hook!" }
+FSL::Inbound.add_event_hook(:CHANNEL_CREATE) {|event| FSR::Log.info "*** [#{event.content[:unique_id]}} Channel created - greetings from the hook!" }
 
 # Start FSR Inbound Listener
-FSR.start_ies!(IesDemo, :host => "localhost", :port => 8021)
+FSR.start_ies!(FSL::Inbound, :host => "localhost", :port => 8021)
 
