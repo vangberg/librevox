8b3b3a47e34eed98f267f6bb9089abe883f96b77
Jayson Vaughn <jayson@onedrop.home>
2009-04-14 16:47:05 -0500

    FSR::App::Read#sendmsg now joins arguments using " " not "|" :)

--- lib/fsr/app/read.rb
+++ lib/fsr/app/read.rb
@@ -12,7 +12,7 @@ module FSR
       end
 
       def sendmsg
-        "call-command: execute\nexecute-app-name: %s\nexecute-app-arg: %s\nevent-lock:true\n\n" % [app_name, arguments.join("|")]
+        "call-command: execute\nexecute-app-name: %s\nexecute-app-arg: %s\nevent-lock:true\n\n" % [app_name, arguments.join(" ")]
       end
 
     end
