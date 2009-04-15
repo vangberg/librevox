e732be4d1b786cdf4c7eeae9fb1f8435529cefd7
Jayson Vaughn <jayson@onedrop.home>
2009-04-15 08:09:10 -0500

    Added FSR::App::UuidGetVar
      * use uuid_getvar("uuid", "varaible") to retreive a channel variable

---
+++ lib/fsr/app/uuid_getvar.rb
@@ -0,0 +1,23 @@
+require "fsr/app"
+module FSR
+  #http://wiki.freeswitch.org/wiki/Mod_commands#uuid_getvar
+  module App
+    class UuidGetVar < Application
+      def initialize(uuid, variable)
+        @uuid = uuid # Unique channel ID
+        @var = var # Channel variable you wish to 'get'
+      end
+
+      def arguments
+        [@uuid, @var]
+      end
+
+      def sendmsg
+        "call-command: execute\nexecute-app-name: %s\nexecute-app-arg: %s\nevent-lock:true\n\n" % [app_name, arguments.join(" ")]
+      end
+
+    end
+
+    register(:uuid_getvar, UuidGetVar)
+  end
+end
