846506901b848becc782788da6e58da3cc1b5d40
foo <jvaughn@hellspass.nationwide-recovery.com>
2009-04-16 09:43:50 -0500

    Refactored FSR::Listener::Outbound   #session_initiated no longer takes a parameter.  use @session for the session object   #update_session now fully updates @session

--- lib/fsr/listener/outbound.rb
+++ lib/fsr/listener/outbound.rb
@@ -7,6 +7,7 @@ module FSR
   load_all_applications
   module Listener
     class Outbound < EventMachine::Protocols::HeaderAndContentProtocol
+      attr_reader :session
 
       # Include FSR::App to get all the applications defined as methods
       include FSR::App
@@ -15,40 +16,23 @@ module FSR
       SENDMSG_METHOD_DEFINITION = "def %s(*args, &block); sendmsg super; end"
       APPLICATIONS.each { |app, obj| module_eval(SENDMSG_METHOD_DEFINITION % app.to_s) }
 
-      def post_init
-        @session = nil # holds the session object
-        send_data("connect\n\n")
-        FSR::Log.debug "Accepting connections."
+      # session_initiated is called when a @session is first created.
+      # Overwrite this in your worker class with the call/channel
+      # handling logic you desire
+      def session_initiated
+        FSR::Log.warn "#{self.class.name}#session_initiated not overwritten"
+        FSR::Log.debug session_data.inspect
       end
 
-      def receive_request(header, content)
-        hash_header = headers_2_hash(header)
-        hash_content = headers_2_hash(content)
-        session = HeaderAndContentResponse.new({:headers => hash_header, :content => hash_content})
-        if @session.nil?
-          @session = session
-          session_initiated(session) 
-        else
-          if session.headers[:event_name].nil?
-            receive_reply(session)
-          else
-            if session.headers[:event_name].to_s.match(/CHANNEL_DATA/i)
-              @session = session
-              receive_reply(HeaderAndConentResponse.new({:headers => {:session => "update"}, :content => {}}))
-            end
-          end
-        end
-      end
-
-      # Received data dispatches the data received by the EM socket,
-      # Either as a Session, a continuation of a Session, or as a Session's last CommandReply
-      
-      def session_initiated(session)
-        session
-      end
-
-      def receive_reply(session)
-        session
+      # receive_reply is called when a response is received.
+      # Overwrite this in your worker class with the call/channel
+      # handling logic you desire, taking @step into account for
+      # state management between commands
+      # @param reply This HeaderAndContent instance will have the channel variables
+      #              in #content, if the session has been updated
+      def receive_reply(reply)
+        FSR::Log.warn "#{self.class.name}#receive_reply not overwritten"
+        FSR::Log.debug reply.inspect
       end
 
       # sendmsg sends data to the EM app socket via #send_data, or
@@ -69,7 +53,43 @@ module FSR
         send_data("api uuid_dump #{@session.headers[:unique_id]}\n\n")
       end
 
-    end
+      def post_init
+        @session = nil # holds the session object
+        send_data("connect\n\n")
+        FSR::Log.debug "Accepting connections."
+      end
 
+      # receive_request is called each time data is received by the event machine
+      # it will manipulate the received data into either a new session or a reply,
+      # to be picked up by #session_initiated or #receive_reply.
+      # If your listener is listening for events, this will also renew your @session
+      # each time you receive a CHANNEL_DATA event.
+      # @param header The header of the request, as passed by HeaderAndContentProtocol
+      # @param content The content of the request, as passed by HeaderAndContentProtocol
+      #
+      # @returns HeaderAndContentResponse
+      def receive_request(header, content)
+        hash_header = headers_2_hash(header)
+        hash_content = headers_2_hash(content)
+        session_header_and_content = HeaderAndContentResponse.new({:headers => hash_header, :content => hash_content})
+        # If we're a new session, call session initiate
+        if @session.nil?
+          @session = session_header_and_content
+          @step = 0
+          session_initiated 
+        elsif session_header_and_content.content[:event_name] # If content includes an event_name, it must be a response from an api command
+          if session_header_and_content.content[:event_name].to_s.match(/CHANNEL_DATA/i) # Anytime we see CHANNEL_DATA event, we want to update our @session
+            session_header_and_content = HeaderAndContentResponse.new({:headers => hash_header.merge(hash_content), :content => {}})
+            @session = session_header_and_content
+            @step += 1
+            receive_reply(hash_header)
+          end
+        else
+          @step += 1
+          receive_reply(session_header_and_content)
+        end
+      end
+
+    end
   end
 end
