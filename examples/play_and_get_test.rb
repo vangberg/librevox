faf4c5ab6612b77d6620bdcf944bb8b1514d5a5b
TJ Vanderpoel <bougy.man@gmail.com>
2009-05-02 16:00:53 -0500

    added Cmd::Calls and a Model::Call to describe call data

--- examples/play_and_get_test.rb
+++ examples/play_and_get_test.rb
@@ -19,7 +19,7 @@ class PlayAndGetTest < FSR::Listener::Outbound
     case @step
     when 1
       FSR::Log.info "*** Reading dtmf for #{exten}"
-      play_and_get_digits "/home/freeswitch/freeswitch/sounds/music/8000/sweet.wav","/home/freeswitch/freeswitch/sounds/en/us/callie/misc/8000/error.wav",4,10,3,10000,["#"],"test", '\d' # play_and_get_digits test
+      play_and_get_digits "tone_stream://%(10000,0,350,440)","/home/freeswitch/freeswitch/sounds/en/us/callie/misc/8000/error.wav",2,10,3,7000,["#"],"test", "\d+" # play_and_get_digits test
     when 2
       FSR::Log.info "*** updating session for #{exten}"
       update_session
