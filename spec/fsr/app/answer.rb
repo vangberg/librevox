55ec1c51d66fa02928b40bb3fccdc3f4c08dc904
Jayson Vaughn <jayson@onedrop.(none)>
2009-05-11 12:14:34 -0500

    New specs now pass

--- spec/fsr/app/answer.rb
+++ spec/fsr/app/answer.rb
@@ -6,7 +6,7 @@ describe "Testing FSR::App::Answer" do
 
   it "answers the incoming call" do
     ans = FSR::App::Answer.new
-    ans.sendmsg.should == "call-command: execute\nexecute-app-name: answer\nexecute-app-arg: \n\n"
+    ans.sendmsg.should == "call-command: execute\nexecute-app-name: answer\n\n"
   end
 
 end
--- spec/fsr/app/fs_sleep.rb
+++ spec/fsr/app/fs_sleep.rb
@@ -4,9 +4,9 @@ FSR::App.load_application("fs_sleep")
 
 describe "Testing FSR::App::FsSleep" do
 
-  it "should put FreeSWITCH leg to sleep" do
-    fs_sleep = FSR::App::FSSleep.new
-    fs_sleep.sendmsg.should == "call-command: execute\nexecute-app-name: break\n\n"
+  it "should put FreeSWITCH leg to sleep for 7000 miliseconds" do
+    fs_sleep = FSR::App::FSSleep.new(7000)
+    fs_sleep.sendmsg.should == "call-command: execute\nexecute-app-name: sleep\nexecute-app-arg: 7000\n\n"
   end
 
 end
