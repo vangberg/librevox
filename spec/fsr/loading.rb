6936e80244783b0dea1fad0c5e2f47a3dbf6381b
Jayson Vaughn <vaughn.jayson@gmail.com>
2009-06-06 16:55:33 -0500

    Added FSR::Cmd::UuidDump and specs

--- spec/fsr/loading.rb
+++ spec/fsr/loading.rb
@@ -16,7 +16,7 @@ describe "Testing FSR module loading methods" do
 
   # When you add commands  you must modify the expected cmds_loaded behavior
   it "Loads all commands" do
-    all_commands = [:originate, :sofia, :fsctl, :sofia_contact, :status, :calls] # If you add a command add it to this set
+    all_commands = [:uuid_dump, :originate, :sofia, :fsctl, :sofia_contact, :status, :calls] # If you add a command add it to this set
     cmds_loaded = FSR.load_all_commands
     cmds_loaded.kind_of?(Array).should == true
     all_commands.each do |cmd|
