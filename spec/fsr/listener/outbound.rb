214be2bf85c46fed659cc4c3cd8521098aaf436b
Jayson Vaughn <vaughn.jayson@gmail.com>
2009-06-06 13:16:29 -0500

    Added em-epecs to test basic functionality of the Outbound Event Socket   * Test that we can establish a new session   * Test we can process a response sent in multiple transmissions   * Test we can receive a reply after a session is created   * Test that we send connect to freeswitch upon a new connection
      TODO:  Need to test Outbound Event socket state machine

--- spec/fsr/listener/outbound.rb
+++ spec/fsr/listener/outbound.rb
@@ -1,15 +1,22 @@
 require "lib/fsr"
 require FSR::ROOT/".."/:spec/:helper
 require FSR::ROOT/:fsr/:listener/:outbound
+gem "tmm1-em-spec"
+require "em/spec"
 
 # Bare class to use for testing
 class MyListener < FSR::Listener::Outbound
-attr_reader :exten
-  def session_initiated
-    @exten = @session.headers[:caller_caller_id_number]
+  attr_accessor :recvd_reply
+
+  def initialize(*args)
+    super(*args)
+    @recvd_reply = []
   end
 
-  def send_data data
+  def session_initiated
+  end
+
+  def send_data(data)
     sent_data << data
   end
 
@@ -17,45 +24,55 @@ attr_reader :exten
     @sent_data ||= ''
   end
 
-end
-
-# helper to instantiate a new MyListener
-def my_listener
-  MyListener.new
-end
-gem "tmm1-em-spec"
-require "em/spec"
-describe "FSR::Listener::Outbound class" do
-
-  it "defines #post_init" do
-    FSR::Listener::Outbound.method_defined?(:post_init).should == true
+  def receive_reply(reply)
+    @recvd_reply << reply
   end
 
 end
 
-EM.describe FSR::Listener::Outbound do
-  should "be true" do
-    done
-  end
-
-  
-end
 
 EM.describe MyListener do
+
   before do
     @listener = MyListener.new(nil)
   end
 
-  should "be able to sendmsg to FreeSWITCH" do
-    @listener.sendmsg("foo")
-    @listener.sent_data.should.equal "connect\n\nsendmsg\nfoo\n"
+  should "send connect to freeswitch upon a new connection" do
+    @listener.receive_data("Content-Length: 0\nCaller-Caller-ID-Number: 8675309\n\n")
+    @listener.sent_data.should.equal "connect\n\n"
     done
   end
 
-  should "be able to receive new session and have an exten" do
-    @listener.receive_data("Content-Length: 0\nCaller-Caller-ID-Number: 8176907937\n\n")
-    @listener.exten.should.equal "8176907937"
+  should "be able to receive a connection and establish a session " do
+    @listener.receive_data("Content-Length: 0\nTest: Testing\n\n")
+    @listener.session.class.should.equal FSR::Listener::HeaderAndContentResponse
     done
   end
+
+  should "be able to read FreeSWITCH channel variables through session" do
+    @listener.receive_data("Content-Length: 0\nCaller-Caller-ID-Number: 8675309\n\n")
+    @listener.session.headers[:caller_caller_id_number].should.equal "8675309"
+    done
+  end
+
+  should "be able to receive and process a response if not sent in one transmission" do
+    @listener.receive_data("Content-Length: ")
+    @listener.receive_data("0\nCaller-Caller-")
+    @listener.receive_data("ID-Number: 8675309\n\n")
+    @listener.session.headers[:caller_caller_id_number].should.equal "8675309"
+    done
+  end
+
+  should "be able to dispatch our receive_reply callback method after a session is already established" do
+    # This should establish the session
+    @listener.receive_data("Content-Length: 0\nTest-Data: foo\n\n")
+
+    # This should be a response, not a session
+    @listener.receive_data("Content-Length: 0\nTest-Reply: bar\n\n")
+
+    @listener.session.headers[:test_data].should.equal 'foo'
+    @listener.recvd_reply.first.headers[:test_reply].should.equal 'bar'
+    done
+  end
+
 end
-
