b3cd01a965d9fb4d629348e498ab2a9457ebcda5
TJ Vanderpoel <TJ Vanderpoel tj@rubyists.com>
2009-08-21 13:48:08 -0500

    pushed the lib, too :)

--- lib/fsr/app/play_and_get_digits.rb
+++ lib/fsr/app/play_and_get_digits.rb
@@ -4,17 +4,32 @@ module FSR
   module App
     class PlayAndGetDigits < Application
       attr_reader :chan_var
+      DEFAULT_ARGS = {:min => 0, :max => 10, :tries => 3, :timeout => 7000, :terminators => ["#"], :chan_var => "fsr_read_dtmf", :regexp => '\d'}
 
-      def initialize(sound_file, invalid_file, min = 0, max = 10, tries = 3, timeout = 7000, terminators = ["#"], chan_var = "fsr_read_dtmf", regexp = "\d")
+      # args format for array:
+      # sound_file, invalid_file, min = 0, max = 10, tries = 3, timeout = 7000, terminators = ["#"], chan_var = "fsr_read_dtmf", regexp = '\d'
+      def initialize(sound_file, invalid_file, *args)
+        puts args.inspect
+        if args.size == 1 and args.first.kind_of?(Hash)
+          arg_hash = DEFAULT_ARGS.merge(args.first)
+        elsif args.size > 0
+          # The array used to .zip the args here can be replaced with DEFAULT_ARGS.keys if Hash keys are ordered (1.9)
+          # For now we'll hard code them to preserve order in 1.8/jruby
+          arg_hash = DEFAULT_ARGS.merge(Hash[[:min, :max, :tries, :timeout, :terminators, :chan_var, :regexp][0 .. (args.size-1)].zip(args)]) 
+        elsif args.size == 0
+          arg_hash = DEFAULT_ARGS
+        else
+          raise "Invalid Arguments for PlayAndGetDigits#new (must pass (sound_file, invalid_file, hash) or (sound_file, invalid_file, min = 0, max = 10, tries = 3, timeout = 7000, terminators = ['#'], chan_var = 'fsr_read_dtmf', regexp = '\d'))"
+        end
         @sound_file = sound_file
         @invalid_file = invalid_file
-        @min = min
-        @max = max
-        @tries = tries
-        @timeout = timeout
-        @chan_var = chan_var
-        @terminators = terminators
-        @regexp = regexp
+        @min = arg_hash[:min]
+        @max = arg_hash[:max]
+        @tries = arg_hash[:tries]
+        @timeout = arg_hash[:timeout]
+        @chan_var = arg_hash[:chan_var]
+        @terminators = arg_hash[:terminators]
+        @regexp = arg_hash[:regexp]
       end
 
       def arguments
