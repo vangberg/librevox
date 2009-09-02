f5a32b531e28fafd759da17d7abb543115540885
Harry Vangberg <harry@vangberg.name>
2009-09-02 14:01:46 +0200

    strip newlines from event.content values

--- lib/fsr/listener/header_and_content_response.rb
+++ lib/fsr/listener/header_and_content_response.rb
@@ -9,6 +9,11 @@ module FSR
       def initialize(args = {})
         @headers = args[:headers]
         @content =  args[:content]
+        strip_newlines
+      end
+
+      def strip_newlines
+        @content.each {|k,v| v.chomp! if v.is_a?(String)}
       end
       
       # Keep backward compat with the other 2 people who use FSR
--- spec/fsr/listener/header_and_content_response.rb
+++ spec/fsr/listener/header_and_content_response.rb
@@ -4,13 +4,22 @@ require 'fsr/listener/header_and_content_response'
 describe FSL::HeaderAndContentResponse do
   HCR = FSL::HeaderAndContentResponse
 
-  it "is initializable with 2 hash arguments" do
+  before do
     header = {:foo => "bar", :jumbo => "shrimp"}
-    content = {:response => "w00t"}
-    hcr = HCR.new({:headers => header, :content => content})
-    hcr.headers.should.include? :foo
-    hcr.content.should.include? :response
+    content = {:response => "w00t\n"}
+    @hcr = HCR.new({:headers => header, :content => content})
   end
+
+  it "is initializable with 2 hash arguments" do
+    @hcr.headers.should.include? :foo
+    @hcr.content.should.include? :response
+  end
+
+  it "strips newlines from content values" do
+    @hcr.content[:response].should == "w00t"
+  end
+end
+
 =begin manveru's future specs
 
   it "can be created with headers" do
@@ -45,4 +54,3 @@ describe FSL::HeaderAndContentResponse do
   end
 
 =end
-end
