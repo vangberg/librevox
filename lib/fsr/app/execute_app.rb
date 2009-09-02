206d68302645ac2b04337d2364d1f6a0ca9fb789
Harry Vangberg <harry@vangberg.name>
2009-09-02 14:48:03 +0200

    rename app -> execute_app. otherwise it royally fucked up.

--- lib/fsr/app/execute_app.rb
+++ lib/fsr/app/execute_app.rb
@@ -2,7 +2,7 @@ require "fsr/app"
 module FSR
   module App
     # http://wiki.freeswitch.org/wiki/Misc._Dialplan_Tools_log
-    class App < Application
+    class ExecuteApp < Application
       attr_reader :app_name, :arguments
       def initialize(app, *args)
         @app_name = app
@@ -10,6 +10,6 @@ module FSR
       end
     end
 
-    register(:app, App)
+    register(:execute_app, ExecuteApp)
   end
 end
