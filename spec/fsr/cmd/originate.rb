9fea2772611883512d02ae4570b21e7f23a3a285
Harry Vangberg <harry@vangberg.name>
2009-12-11 03:22:50 +0100

    kamikaze on cmd::originate

--- spec/fsr/cmd/originate.rb
+++ spec/fsr/cmd/originate.rb
@@ -2,42 +2,16 @@ require 'spec/helper'
 require "fsr/cmd"
 
 describe "Testing FSR::Cmd::Originate" do
-  # Invalid originates
-  it "Must be passed an argument hash" do
-    lambda { FSR::Cmd::Originate.new(nil, :endpoint) }.should.raise(ArgumentError).
-      message.should.match(/args \(Passed: <<<.*?>>>\) must be a hash/)
+  should "originate call to endpoint" do
+    originate = FSR::Cmd::Originate.new("user/bougyman", "4000")
+    originate.raw.should == "api originate user/bougyman 4000"
   end
 
-  it "Should require args[:target_options] to be a hash" do
-    lambda { FSR::Cmd::Originate.new(nil, :endpoint => "4000", :target => "user/bougyman", :target_options => 1) }.should.raise(ArgumentError).
-      message.should.match(/args\[:target_options\] \(Passed: <<<.*?>>>\) must be a hash/)
-  end
+  should "send specified variables" do
+    originate = FSR::Cmd::Originate.new 'user/bougyman', 1234,
+                                        :ignore_early_media => true,
+                                        :other_option => "value"
 
-  it "Can not originate without a target" do
-    lambda { FSR::Cmd::Originate.new(nil, :endpoint => "4000") }.should.raise(ArgumentError).
-      message.should.match(/Cannot originate without a :target set/)
+    originate.raw.should == "api originate {ignore_early_media=true,other_option=value}user/bougyman 1234"
   end
-
-  it "Can not originate without an endpoint" do
-    lambda { FSR::Cmd::Originate.new(nil, :target => "4000") }.should.raise(ArgumentError).
-      message.should.match(/Cannot originate without an :endpoint set/)
-  end
-
-  # Originate to an extension
-  it "Originates calls to extensions" do
-    originate = FSR::Cmd::Originate.new(nil, :target => "user/bougyman", :endpoint => "4000")
-    originate.raw.should == "originate {ignore_early_media=true,originate_timeout=30,origination_caller_id_name=FSR,origination_caller_id_number=8675309}user/bougyman 4000"
-  end
-
-  # Different options choices
-  it "Honors timeout in :timeout option" do
-    originate = FSR::Cmd::Originate.new(nil, :target => "user/bougyman", :timeout => 10, :endpoint => "4000")
-    originate.raw.should == "originate {ignore_early_media=true,originate_timeout=10,origination_caller_id_name=FSR,origination_caller_id_number=8675309}user/bougyman 4000"
-  end
-
-  it "Honors timeout in :target_options[timeout] option" do
-    originate = FSR::Cmd::Originate.new(nil, :target => "user/bougyman", :target_options => {:timeout => 10}, :endpoint => "4000")
-    originate.raw.should == "originate {ignore_early_media=true,originate_timeout=10,origination_caller_id_name=FSR,origination_caller_id_number=8675309}user/bougyman 4000"
-  end
-
 end
