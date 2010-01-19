28c3463f3ea6a84934ddb73aec9c353999c099e2
Harry Vangberg <harry@vangberg.name>
2010-01-19 20:07:42 +0100

    add logger

--- lib/librevox.rb
+++ lib/librevox.rb
@@ -1,9 +1,28 @@
 require 'eventmachine'
+require 'optparse'
+require 'logger'
 require 'librevox/listener/inbound'
 require 'librevox/listener/outbound'
 
 module Librevox
-  VERSION = "0.1.1"
+  VERSION   = "0.1.1"
+
+  def self.options
+    @options ||= {
+      :log_file   => STDOUT,
+      :log_level  => Logger::INFO
+    }
+  end
+
+  def self.logger
+    @logger || logger!
+  end
+
+  def self.logger!
+    logger = Logger.new(options[:log_file])
+    logger.level = options[:log_level]
+    logger
+  end
 
   # When called without a block, it will start the listener that is passed as
   # first argument:
@@ -17,6 +36,7 @@ module Librevox
   #     run OtherListner
   #   end
   def self.start(klass=nil, args={}, &block)
+    logger.info "Starting Librevox"
     EM.run {
       block_given? ? instance_eval(&block) : run(klass, args)
     }
